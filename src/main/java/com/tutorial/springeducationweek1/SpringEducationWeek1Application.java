2599fd300e9b242b42cdedeb13d806c770b2e7ce
chahyeonho <chamym@naver.com>
2025-06-16 18:30:08 +0900

    init commit

---
+++ src/main/java/com/tutorial/springeducationweek1/SpringEducationWeek1Application.java
@@ -0,0 +1,13 @@
+package com.tutorial.springeducationweek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEducationWeek1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEducationWeek1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutorial/springeducationweek1/SpringEducationWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.springeducationweek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEducationWeek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
