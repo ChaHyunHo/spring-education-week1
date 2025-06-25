0abfcc5130bd4b92d4d4fb30557bab02c1b73b09
chahyeonho <chamym@naver.com>
2025-06-25 23:54:02 +0900

    2주차 3일차 실습 추가

--- src/main/java/com/tutorial/springeducationweek1/domain/product/mapper/ProductMapper.java
+++ src/main/java/com/tutorial/springeducationweek1/domain/product/mapper/ProductMapper.java
@@ -2,7 +2,6 @@ package com.tutorial.springeducationweek1.domain.product.mapper;
 
 import com.tutorial.springeducationweek1.domain.product.dto.ProductCreateRequest;
 import com.tutorial.springeducationweek1.domain.product.dto.ProductSearchResponse;
-import com.tutorial.springeducationweek1.domain.product.dto.ProductUpdateRequest;
 import com.tutorial.springeducationweek1.domain.product.entity.Product;
 import org.mapstruct.Mapper;
 
@@ -12,6 +11,4 @@ public interface ProductMapper {
   ProductSearchResponse toSearch(Product product);
 
   Product toEntity(ProductCreateRequest request);
-
-  Product toEntity(ProductUpdateRequest request);
 }
